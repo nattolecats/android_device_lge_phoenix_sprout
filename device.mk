fe537b679f98f522b7c9c701e0fb9ebb86f95d84
Han Sol Jin <hansol@hansol.ca>
2024-03-03 11:53:16 -0800

    phoenix_sprout: don't build AOSP gatekeeper/keymaster
    This device needs the QCOM one.
    
    Change-Id: Iaf87696bf799c297dbdb3c016cf42610cfd6f78f

--- device.mk
+++ device.mk
@@ -220,8 +220,7 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0.vendor
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -299,8 +298,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.0.vendor
 
 # Lights
 PRODUCT_PACKAGES += \
